6f2bf5fd2b9e0d4f9bd10f7a90da38df0ab00663
melanylibia <elibialany@gmail.com>
2025-02-08 09:53:31 -0400

    feat: added POST request

--- db/hasura/listOfUser/src/UserList.jsx
+++ db/hasura/listOfUser/src/UserList.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import React, { useState } from 'react';
+import { useQuery, useMutation, gql } from '@apollo/client';
 
 const GET_USERS = gql`
   query GetUsers {
@@ -11,14 +11,66 @@ const GET_USERS = gql`
   }
 `;
 
+const ADD_USER = gql`
+  mutation AddUser($objects: [users_insert_input!]!) {
+    insert_users(objects: $objects) {
+      affected_rows
+      returning {
+        id
+        name
+        email
+      }
+    }
+  }
+`;
+
+
 function UserList() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const [addUser] = useMutation(ADD_USER, {
+    refetchQueries: [{ query: GET_USERS }],
+  });
+
   const { loading, error, data } = useQuery(GET_USERS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error ...</p>;
 
+  const handleSubmit = async (event) => {
+
+    event.preventDefault();
+
+    try {
+
+      await addUser({
+        variables:
+        {
+          objects:
+            [{
+              name,
+              email,
+            }],
+        },
+      });
+      console.log('User added successfully!');
+      setName('');
+      setEmail('');
+    } catch (error) {
+      console.error('Error adding user:', error);
+    }
+  };
+
+
   return (
     <div>
+      <h1>Add User</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+        <button type="submit">Add User</button>
+      </form>
       <h1>Users</h1>
       <ul>
         {data.users.map(user => (
