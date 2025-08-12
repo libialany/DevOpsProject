696514afea60466165c19ae085d6f7c606de7026
mestevez <mestevez@bancosol.com.bo>
2025-08-12 10:38:52 -0400

    feat: simple description

--- terraform/variables.tf
+++
@@ -1,8 +0,0 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-variable "aws_ami_id" {
-  description = "ami id"
-  type        = string
-  default     = "ami-020cba7c55df1f615"
-}
