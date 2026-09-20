769ba97054d2069802e56e1715b6d12ce63a92a0
libialany <elibialany@gmail.com>
2026-09-20 10:25:38 -0400

    feat: added topics

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+
+app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
+
+@app.get("/")
+def hello():
+    return {"message": "Hello from Kubernetes!"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
