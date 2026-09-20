64c2d12248c9f7f063619841b92ccde6488c2842
libialany <elibialany@gmail.com>
2026-09-19 22:38:57 -0400

    feat: added fiels

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,30 @@
+variable "name" {
+    description = ""
+    type = string
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "Availability zones to spread subnets across"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets, one per AZ"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets, one per AZ"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Tags applied to every resource"
+  type        = map(string)
+  default     = {}
+}
