dc5ac593491f64bce501a55050ff57b34a643422
libialany <elibialany@gmail.com>
2026-09-20 12:13:25 -0400

    feat: v1 helm

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
-Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def hello():
